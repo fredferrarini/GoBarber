6b40976f1fcc49d801972b307f84e50ad79b66da
Frederico Ferrarini <fred.ferrarini@gmail.com>
2020-11-02 06:56:14 +0100

    final version from class

--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 
 import signUpBackgroundImg from '../../assets/sign-up-background.png';
@@ -14,11 +14,29 @@ export const Content = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  place-content: center;
-
+  justify-content: center;
   width: 100%;
   max-width: 700px;
+`;
+
+const appearFromRight = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(50px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromRight} 1s;
 
   form {
     margin: 80px 0;
@@ -29,7 +47,7 @@ export const Content = styled.div`
       margin-bottom: 24px;
     }
 
-    /* a {
+    a {
       color: #f4ede8;
       display: block;
       margin-top: 24px;
@@ -39,10 +57,10 @@ export const Content = styled.div`
       &:hover {
         color: ${shade(0.2, '#F4EDE8')};
       }
-    } */
+    }
   }
   > a {
-    color: #f4ede8;
+    color: #ff9000;
     display: block;
     margin-top: 24px;
     text-decoration: none;
@@ -52,7 +70,7 @@ export const Content = styled.div`
     align-items: center;
 
     &:hover {
-      color: ${shade(0.2, '#f4ede8')};
+      color: ${shade(0.2, '#ff9000')};
     }
 
     svg {
